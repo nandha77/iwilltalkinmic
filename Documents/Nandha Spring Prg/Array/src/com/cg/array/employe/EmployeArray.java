22c839340d1fd9704579df13a415815938c46485
Nandha77 <nandhanivin771999@gmail.com>
2021-01-28 14:37:16 +0530

    first commit

---
+++ Documents/Nandha Spring Prg/Array/src/com/cg/array/employe/EmployeArray.java
@@ -0,0 +1,33 @@
+package com.cg.array.employe;
+
+public class EmployeArray {
+	
+	private int empId;
+	private String empName;
+	
+	public EmployeArray(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+	
+
+}
